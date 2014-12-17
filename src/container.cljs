38d9f9ac9cf36926bfce877206afed0071e93590
Teemu Tuulari <ttuulari@gmail.com>
2014-12-17 11:51:37 +0200

    Indent

--- src/container.cljs
+++ src/container.cljs
@@ -15,54 +15,54 @@
 
 (defn render-container [app]
   (d/div
-      {:class "grids-examples"}
-      (om/build navbar/navbar-view true)
-      (g/grid
-       {}
-       (g/row {:class "show-grid"}
-              (g/col {:xs 6 :md 4}
-                     (d/div {:class "summary-container"}
-                            (om/build summary/portfolio-summary-view app)))
-              (g/col {:xs 12 :md 8}
-                     (d/div {:class "graph-slider"}
-                            (g/row {:class "show-grid"}
-                                   (g/col {:xs 13 :md 8}
-                                          (om/build graph/graph-view
-                                                    (graph/graph-input-data app (graph/app-state->date-labels app))
-                                                    {:opts
-                                                     {:js          {:className "ct-chart portfolio-graph"
-                                                                    :xs 8 :md 4}
-                                                      :constructor (.-Line js/Chartist)
-                                                      :graph-opts  {:width 600
-                                                                    :height 300
-                                                                    :lineSmooth false
-                                                                    :showPoint false
-                                                                    :chartPadding 20
-                                                                    :axisX {
-                                                                            :showLabel true}
-                                                                    :axisY {
-                                                                            :scaleMinSpace 50
-                                                                            :labelInterpolationFnc (fn [value]
-                                                                                                     (util/to-fixed value 2))}}}})
-                                          (om/build slider/slider-view
-                                                    {:length (get-in app [:selected-date :total-length])
-                                                     :range  (get-in app [:selected-date :range])}
-                                                    {:opts
-                                                     {:js
-                                                      {:className "slider show slider-material-indigo"}}}))
+   {:class "grids-examples"}
+   (om/build navbar/navbar-view true)
+   (g/grid
+    {}
+    (g/row {:class "show-grid"}
+           (g/col {:xs 6 :md 4}
+                  (d/div {:class "summary-container"}
+                         (om/build summary/portfolio-summary-view app)))
+           (g/col {:xs 12 :md 8}
+                  (d/div {:class "graph-slider"}
+                         (g/row {:class "show-grid"}
+                                (g/col {:xs 13 :md 8}
+                                       (om/build graph/graph-view
+                                                 (graph/graph-input-data app (graph/app-state->date-labels app))
+                                                 {:opts
+                                                  {:js          {:className "ct-chart portfolio-graph"
+                                                                 :xs 8 :md 4}
+                                                   :constructor (.-Line js/Chartist)
+                                                   :graph-opts  {:width 600
+                                                                 :height 300
+                                                                 :lineSmooth false
+                                                                 :showPoint false
+                                                                 :chartPadding 20
+                                                                 :axisX {
+                                                                         :showLabel true}
+                                                                 :axisY {
+                                                                         :scaleMinSpace 50
+                                                                         :labelInterpolationFnc (fn [value]
+                                                                                                  (util/to-fixed value 2))}}}})
+                                       (om/build slider/slider-view
+                                                 {:length (get-in app [:selected-date :total-length])
+                                                  :range  (get-in app [:selected-date :range])}
+                                                 {:opts
+                                                  {:js
+                                                   {:className "slider show slider-material-indigo"}}}))
 
-                                   (g/col {:xs 5 :md 3}
-                                          (graph/build-legend (:components app))))))
-          (g/row {:class "show-grid"}
-                 (g/col {:xs 6 :md 4}
-                        (d/div {:class "search-container"}
-                               (om/build input/input-view true)
-                               (om/build search-results/results-view app)))
+                                (g/col {:xs 5 :md 3}
+                                       (graph/build-legend (:components app))))))
+           (g/row {:class "show-grid"}
+                  (g/col {:xs 6 :md 4}
+                         (d/div {:class "search-container"}
+                                (om/build input/input-view true)
+                                (om/build search-results/results-view app)))
 
                   (g/col {:xs 10 :md 6}
                          (om/build range-buttons/range-buttons-view
                                    (:selected-date app))))
 
-       (d/div {:class "container portfolio-container"}
-              (g/row {:class "portfolio-container-row"}
-                     (om/build components/portfolio-list-view app)))))))
+           (d/div {:class "container portfolio-container"}
+                  (g/row {:class "portfolio-container-row"}
+                         (om/build components/portfolio-list-view app)))))))
