1fccdac56cd541a843d6f99c59fd64a1cdd44c61
Teemu Tuulari <teemu.tuulari@greyarealabs.com>
2015-01-08 12:15:06 +0200

    Graph cleanup

--- src/graph.cljs
+++ src/graph.cljs
@@ -14,29 +14,33 @@
       :dates))
 
 (defn window-input
+  "Window input sequence by selected date and date labels."
   [date-labels selected-date input]
   (let [sorted-labels (apply sorted-set date-labels)
-        sub-labels    (subseq (apply sorted-set date-labels)
+        sub-labels    (subseq sorted-labels
                               <=
                               (:end-date selected-date))
         to-take       (Math/abs (:range selected-date))]
     (take-last to-take (take (count sub-labels) input))))
 
-(defn construct-prices [labels prices sel-prices]
-    (if (empty? prices)
-      {:labels   labels
-       :series   [(repeat (count labels) 0.0)]}
-      (let [cat-prices     (concat [(apply map + prices)] sel-prices)
-            all-prices     (filter (complement price/all-zeros?) cat-prices)
-            scaled-prices  (map util/scale-seq all-prices)]
+(defn construct-prices
+  "Construct graphable data based on labels, combined prices and selected prices."
+  [labels prices sel-prices]
+  (if (empty? prices)
+    {:labels   labels
+     :series   [(repeat (count labels) 0.0)]}
+    (let [cat-prices     (concat [(apply map + prices)] sel-prices)
+          all-prices     (filter (complement price/all-zeros?) cat-prices)
+          scaled-prices  (map util/scale-seq all-prices)]
 
-        (if (> (count all-prices) 1)
-          {:labels   labels
-           :series   scaled-prices}
-          {:labels   labels
-           :series   all-prices}))))
+      (if (> (count all-prices) 1)
+        {:labels   labels
+         :series   scaled-prices}
+        {:labels   labels
+         :series   all-prices}))))
 
 (defn graph-input-data
+  "Transform data into plottable data."
   [input date-labels]
   (let [window-labels (window-input date-labels (:selected-date input) date-labels)
         labels        (take-nth (/ (count window-labels)
@@ -64,6 +68,7 @@
     (construct-prices labels prices sel-prices)))
 
 (defn draw [element input-data opts]
+  "Draw the JS chart"
   (let [constructor  (:constructor opts)]
     (constructor.
       element
@@ -72,24 +77,30 @@
       (clj->js (:responsive-opts opts)))))
 
 (defn output-legends [present selected]
+  "Construct graph legend text."
   (if (empty? present)
     selected
     (concat ["Portfolio"] selected)))
 
 (defn build-legend [components]
-  (let [name-mapper     (map (fn [[a-name _]] a-name))
-        amount-filter   (filter (fn [[_ elem]] (> (:amount elem) 0)))
-        selected-filter (filter (fn [[_ elem]] (:selected elem)))
+  "Build graph legend div using transducers."
+  (let [name-mapper     (map (fn [[a-name _]]
+                               a-name))
+        amount-filter   (filter (fn [[_ elem]]
+                                  (> (:amount elem) 0)))
+        selected-filter (filter (fn [[_ elem]]
+                                  (:selected elem)))
         name-amount     (comp amount-filter name-mapper)
         name-selected   (comp selected-filter name-mapper)
-        present         (transduce name-amount conj [] components)
-        selected        (transduce name-selected conj [] components)]
-  (d/div {:class "ct-legend list-group"}
-         (map-indexed (fn [index legend]
-                        (d/li {:class (str "list-group-item ct-legend-text ct-series-" index)} legend))
-                      (output-legends present selected)))))
+        present         (into [] name-amount components)
+        selected        (into [] name-selected components)]
+    (d/div {:class "ct-legend list-group"}
+           (map-indexed (fn [index legend]
+                          (d/li {:class (str "list-group-item ct-legend-text ct-series-" index)} legend))
+                        (output-legends present selected)))))
 
 (defn graph-view [app owner opts]
+  "Graph Om component."
   (reify
     om/IDidMount
     (did-mount [this]
