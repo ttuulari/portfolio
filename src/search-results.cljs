e027054b8730e582a60d4ce7224a22345240cf38
Teemu Tuulari <ttuulari@gmail.com>
2015-01-08 16:26:28 +0200

    Search results uses om-tools. Minor cleanup.

--- src/search-results.cljs
+++ src/search-results.cljs
@@ -2,62 +2,72 @@
   (:require
     [cljs.core.async :as async
       :refer [<! >! chan put! sub]]
+   [portfolio.util :as util]
     [om.core :as om :include-macros true]
-    [om.dom :as dom :include-macros true])
+    [om-tools.dom :as d :include-macros true])
   (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
-(defn prefix? [to-test prefix]
-  (= (.indexOf (.toLowerCase to-test) (.toLowerCase prefix)) 0))
+(defn prefix?
+  "Predicate for testing lowercase prefix."
+  [to-test prefix]
+  (= (.indexOf (.toLowerCase to-test)
+               (.toLowerCase prefix))
+     0))
 
-(defn prefix->strs [prefix str-list]
+(defn prefix->strs
+  "Filter results by prefix."
+  [prefix str-list]
   (let [pre?     (fn [elem] (prefix? elem prefix))
         filtered (filter pre? str-list)]
     (if (= (count filtered) (count str-list))
       []
       (vec filtered))))
 
-(defn result-view [app owner]
+(defn result-view
+  "Search result om component."
+  [app owner]
   (reify
     om/IRenderState
-    (render-state [this state]
-      (dom/a
-       #js {:className   "list-group-item search-result"
-            :onClick     (fn []
-                           (put!
-                            (:search-chan (om/get-shared owner))
-                            {:topic :search-click
-                             :value app}))}
-       app))))
+    (render-state
+     [this state]
+     (d/a
+      {:class        "list-group-item search-result"
+       :on-click     (fn []
+                       (put!
+                        (:search-chan (om/get-shared owner))
+                        {:topic :search-click
+                         :value app}))}
+      app))))
 
-(defn result-separator-view [app owner]
-  (reify
-    om/IRenderState
-    (render-state [this state]
-      (dom/div #js {:className "list-group-separator"}))))
-
-(defn results-view [app owner]
+(defn results-view
+  "List of search results om component."
+  [app owner]
   (reify
     om/IWillMount
-    (will-mount [this]
-      (let [search-chan (sub (:notif-chan (om/get-shared owner)) :search (chan) false)
-            click-chan  (sub (:notif-chan (om/get-shared owner)) :search-click (chan) false)]
-        (go-loop []
-          (let [[v c] (alts! [search-chan click-chan]
-                             {:as {:default true}})
-                reset-results    (fn [_] [])
-                update-results   (fn [_]
-                                   (prefix->strs (:value v) (keys (:data @app))))]
-            (condp = c
-              search-chan   (om/transact! app :results update-results)
-              click-chan    (om/transact! app :results reset-results))
-              :default      nil
-            (recur)))))
+    (will-mount
+     [this]
+     (let [search-chan (sub (:notif-chan (om/get-shared owner)) :search (chan) false)
+           click-chan  (sub (:notif-chan (om/get-shared owner)) :search-click (chan) false)]
+       (go-loop
+        []
+        (let [[v c] (alts! [search-chan click-chan]
+                           {:as {:default true}})
+              reset-results    (fn [_] [])
+              update-results   (fn [_]
+                                 (prefix->strs (:value v)
+                                               (keys (:data @app))))]
+          (condp = c
+            search-chan   (om/transact! app :results update-results)
+            click-chan    (om/transact! app :results reset-results))
+          :default      nil
+          (recur)))))
 
     om/IRenderState
-    (render-state [this state]
-      (apply dom/div #js {:className "list-group search-results"}
-             (om/build-all
-              result-view
-              (map
-               (fn [elem] elem)
-               (:results app)))))))
+    (render-state
+     [this state]
+     (d/div {:class "list-group search-results"}
+            (om/build-all
+             result-view
+             (map
+              (fn [elem] elem)
+              (:results app)))))))
