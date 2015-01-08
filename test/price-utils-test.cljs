b1915b98dd81b8d733c8e80c2942c850139836e0
Teemu Tuulari <ttuulari@gmail.com>
2015-01-08 21:06:32 +0200

    Yield test

---
+++ test/price-utils-test.cljs
@@ -0,0 +1,10 @@
+(ns portfolio.price-utils.test
+  (:require-macros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]
+            [portfolio.price-utils :as p]))
+
+(deftest yield-no-change
+  (is (=
+       (p/yield 1 1)
+       0)))
