0049a95ca77b46a11c0b23af40f2b64f9de4b48e
Teemu Tuulari <ttuulari@gmail.com>
2015-01-09 09:46:12 +0200

    Price utils tests.

--- test/price-utils-test.cljs
+++ test/price-utils-test.cljs
@@ -8,3 +8,37 @@
   (is (=
        (p/yield 1 1)
        0)))
+
+(deftest yield-halves
+  (is (=
+       (p/yield 2 1)
+       -50)))
+
+(deftest yield-doubles
+  (is (=
+       (p/yield 1 2)
+       100)))
+
+(deftest sample-variance-zero
+  (is (=
+       (p/sample-variance [1 1])
+       0)))
+
+(deftest sample-variance-half
+  (is (=
+       (p/sample-variance [1 2])
+       0.5)))
+
+(deftest std-is-root-variance
+  (is (=
+       (Math.pow (p/sample-variance [1 2]) 0.5)
+       (p/sample-std [1 2]))))
+
+(deftest return-ratio-is-last-first-yield
+  (let [yield-ratio    (->
+                        (p/yield 2 1)
+                        (+ 100)
+                        (/ 100))]
+  (is (=
+       yield-ratio
+       (first (p/return-ratio [2 1]))))))
